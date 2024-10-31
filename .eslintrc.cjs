9aeecfe6ecab75a38397f855e2447a66ca05ffdc
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-10-31 10:08:46 +0200

    Configure eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,51 @@
+/**  @type {import("eslint").Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    "react-native/react-native": true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:react-native/all",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "react-native",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "node_modules",
+    ".expo",
+    ".expo-shared",
+    "assets",
+    "*.config.js",
+  ],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "react-native/no-raw-text": "off",
+    "react-native/no-color-literals": "off",
+    "react-native/no-inline-styles": "off",
+    "react/prop-types": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};

4b154bfc7a3447fa761143583368c8ecba135355
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-11-11 14:13:13 +0200

    Simplify navigation and use Material 3 theme across the app (#27)
    * Exclude android and ios directories from git
    
    * Simplify navigation and use Material3 theme across the app
    
    * Exclude android and ios directories from git
    
    * setup SQLite db with 'exercises' and 'route_points' tables
    
    * add some basic exercise database queries
    
    * add some basic route_points database queries
    
    * add database exports index file
    
    * combine start/finish exercise into addExercise
    
    * Move database stuff out of /app
    
    ---------
    
    Co-authored-by: Aki Pyykkönen <aki.pyykkonen@me.com>

---
+++ app/index.tsx
@@ -0,0 +1,79 @@
+import { useRouter } from "expo-router";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Appbar, FAB, useTheme, Text } from "react-native-paper";
+import Toast from "react-native-root-toast";
+import { initDB } from "@/database";
+
+export default function HomeScreen() {
+  const router = useRouter();
+  const theme = useTheme();
+  // FIXME: Move to Context
+  const [isTracking, setIsTracking] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await initDB();
+      console.log("Database initialized");
+    })();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Appbar.Header>
+        <Appbar.Content
+          title={
+            <Text variant="headlineMedium">
+              <Text style={{ ...styles.appTitle, color: "white" }}>Pace</Text>
+              <Text style={{ ...styles.appTitle, color: "orange" }}>Mate</Text>
+            </Text>
+          }
+        />
+        <Appbar.Action icon="cog" onPress={() => router.push("/settings")} />
+      </Appbar.Header>
+      <View style={styles.content}>
+        <FAB
+          icon={isTracking ? "stop" : "record"}
+          label={isTracking ? "Stop" : "Start"}
+          onPress={() => {
+            if (isTracking) {
+              Toast.show("Long press to stop", {
+                duration: Toast.durations.SHORT,
+              });
+            } else {
+              setIsTracking(true);
+            }
+          }}
+          // Using long press prevents accidentally stopping the tracking
+          onLongPress={() => setIsTracking(false)}
+          color={isTracking ? theme.colors.onError : theme.colors.onPrimary}
+          style={{
+            ...styles.fab,
+            backgroundColor: isTracking
+              ? theme.colors.error
+              : theme.colors.primary,
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appTitle: {
+    fontStyle: "italic",
+    fontWeight: "bold",
+  },
+  content: {
+    flex: 1,
+    gap: 16,
+    overflow: "hidden",
+    padding: 32,
+  },
+  fab: {
+    bottom: 0,
+    margin: 16,
+    position: "absolute",
+    right: 0,
+  },
+});
